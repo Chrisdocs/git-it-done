04e560729efe4fc401ce32200611e7455df4ea7c
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-04-21 16:17:18 -0500

    error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,19 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var queryString = document.location.search;
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+    } else {
+        document.location.replace("./index.html");
+    }
+}
+
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,14 +24,16 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 //pass response data to DOM function
                 displayIssues(data);
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
-            alert("There was a problem with your request");
+            // if not successful redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 }
-
-getRepoIssues("chrisdocs/robot-gladiators");
 
 var displayIssues = function(issues) {
 
@@ -54,3 +71,18 @@ var displayIssues = function(issues) {
         issueContainerEl.appendChild(issuesEl);
     }
 };
+
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit: ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+}
+
+getRepoName();
